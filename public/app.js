c713e694e7688e2688160c19ae888b56d4c501c5
Abenezer Abebe <abenezerabebe@gmail.com>
2017-02-23 23:07:29 +0100

    Adjusted controller and views

--- public/app.js
+++ public/app.js
@@ -1,12 +1,26 @@
 (function() {
 	'use strict';
 
-	const spots = angular.module('spots', []);
+	const spots = angular.module('spots', ['ngRoute']);
 
 	spots.controller('spotsController', function($scope, $http) {
 		$http.get('http://localhost:3000/api/spots/city')
 			.then(function(response) {
 				$scope.spots = response.data;
 			});
+	});
+
+	spots.config(function($routeProvider, $locationProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: 'home.html',
+			// controller: 'spotsController',
+		})
+		.when('/spots', {
+			templateUrl: 'spots.html',
+			controller: 'spotsController',
+		});
+
+		$locationProvider.html5Mode(true);
 	});
 })();
