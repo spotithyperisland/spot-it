30ebc6736b130e469197d2dadbf5b5184f4edd21
Abenezer Abebe <abenezerabebe@gmail.com>
2017-02-25 17:43:25 +0100

    Created search field and pushing search directly to backend

--- public/app.js
+++ public/app.js
@@ -3,18 +3,67 @@
 
 	const spots = angular.module('spots', ['ngRoute']);
 
-	spots.controller('spotsController', function($scope, $http) {
-		$http.get('http://localhost:3000/api/spots/city')
-			.then(function(response) {
-				$scope.spots = response.data;
-			});
+
+	spots.controller('homeController', ['$scope', function($scope) {
+		$scope.tagline = 'Explore';
+	}]);
+
+
+	spots.service('userService', function UserService() {
+		this.save = function(name, email, term, city) {
+			this.name = name;
+			this.email = email;
+			this.term = term;
+			this.city = city;
+		};
+
+		this.getUser = function() {
+			return this.term;
+		};
 	});
+
+
+	spots.controller('searchController', ['$scope', 'userService',
+		function($scope, userService) {
+			$scope.master = {};
+
+			$scope.update = function(user) {
+				$scope.master = angular.copy(user);
+				userService.save(user.name, user.email, user.term, user.city);
+			};
+
+			$scope.reset = function() {
+				$scope.user = angular.copy($scope.master);
+			};
+
+			$scope.reset();
+		}]);
+
+
+	spots.controller('spotsController', ['$scope', '$http', 'userService',
+		function($scope, $http, userService) {
+			console.log(userService.getUser());
+
+			$scope.getSpots = function() {
+				const term = userService.getUser();
+
+				$http.get('http://localhost:3000/api/spots/' + term)
+				.then(function(response) {
+					$scope.spots = response.data;
+				});
+			};
+		}]);
+
 
 	spots.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 		.when('/', {
 			templateUrl: 'home.html',
-			// controller: 'spotsController',
+			controller: 'homeController',
+		})
+		.when('/search-flow', {
+			templateUrl: 'search-flow.html',
+			controller: 'searchController',
 		})
 		.when('/spots', {
 			templateUrl: 'spots.html',
